79180f781ce44c2d9357c9de430de163833f5075
Jawad Rashid <jawadrashid2010@gmail.com>
2017-09-11 05:18:55 +0500

    - Two more asteriods type added and hazards code changed to use array to randomly choose a hazard to use. - Added StarField particle effect - Added background scrolling by using two backgrounds and script - Added enemy ship and implemented all logic for it including movement, contact and shooting - Added dodging for enemy ships as well.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -13,23 +13,33 @@ public class DestroyByContact : MonoBehaviour {
 	void Start()
 	{
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
-		if (gameControllerObject != null) {
+		if (gameControllerObject != null) 
+		{
 			gameController = gameControllerObject.GetComponent<GameController> ();
 		}
-		if (gameController == null) {
+		if (gameController == null) 
+		{
 			Debug.Log ("Cannot find 'GameController' script");
 		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
+		if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+		{
 			return;
 		}
-		Instantiate (explosion, transform.position, transform.rotation);
-		if (other.tag == "Player") {
+
+		if (explosion != null) 
+		{
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+
+		if (other.CompareTag("Player")) 
+		{
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 			gameController.GameOver ();
 		}
+
 		gameController.AddScore (scoreValue);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
